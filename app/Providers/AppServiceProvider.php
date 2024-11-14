ee06fd15f3a7d2a52cba2f11aace1bd40b89aebc
Rao Talha <talharao997az@gmail.com>
2024-11-14 23:29:25 +0500

    service propvider remove!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,11 +21,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('*', function ($view) {
-            if (Auth::check()) {
-                $notifications = Auth::user()->unreadNotifications;
-                $view->with('notifications', $notifications);
-            }
-        });
     }
 }
